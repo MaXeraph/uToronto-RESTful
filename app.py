3990280a4ace71b59d8b6600b4e655a6ed73a11b
Minh Khoi Pham <max.pham@mail.utoronto.ca>
2019-03-03 21:09:00 -0500

    testing new format

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from todo import Todo
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(Todo, "/GET/<str:code>")
+api.add_resource(Todo, "/GET/<code>")
 
 if __name__ == "__main__":
     app.run()
