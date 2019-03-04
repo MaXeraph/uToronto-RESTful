d2b11afb0c1fb065c29e55c56c518926f6db30d4
Minh Khoi Pham <max.pham@mail.utoronto.ca>
2019-03-03 20:59:11 -0500

    GET implemented: GET/CSC209

--- todo.py
+++ todo.py
@@ -1,34 +1,30 @@
 from flask_restful import Resource
+import sqlite3
+import os
 
-todos = [
-  {
-    "id": 1,
-    "item": "Create sample app",
-    "status": "Completed"
-  },
-  {
-    "id": 2,
-    "item": "Deploy in Heroku",
-    "status": "Open"
-  },
-  {
-    "id": 3,
-    "item": "Publish",
-    "status": "Open"
-  }
-]
+fileDir = os.path.dirname(os.path.realpath('__file__'))
+
+#For accessing the file in a folder contained in the current folder
+
+# Creates or opens a file called uToronto with a SQLite3 DB
+db = sqlite3.connect(os.path.join(fileDir, 'data\\uToronto.db'))
+cursor = db.cursor()
 
 class Todo(Resource):
-    def get(self, id):
-        for todo in todos:
-            if(id == todo["id"]):
-                return todo, 200
-        return "Item not found for the id: {}".format(id), 404
+    # def get(self, id):
+    #     for todo in todos:
+    #         if(id == todo["id"]):
+    #             return todo, 200
+    #     return "Item not found for the id: {}".format(id), 404
 
-    def put(self, id):
-        for todo in todos:
-            if(id == todo["id"]):
-                todo["item"] = request.form["data"]
-                todo["status"] = "Open"
-                return todo, 200
-        return "Item not found for the id: {}".format(id), 404
+    def get(self, code):
+        try:
+            cursor.execute('''SELECT courseCode, title, hours, summary,
+                                    prerecquisites, exclusions, distribution,
+                                    breadth, program FROM courses WHERE courseCode=?''', (code,))
+            course = cursor.fetchone()
+            return course, 200
+        except Exception as e:
+            # Roll back any change if something goes wrong
+            db.rollback()
+            return "Item not found for the id: {}".format(id), 404
