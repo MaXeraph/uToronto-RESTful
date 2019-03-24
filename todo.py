2f4573cc79c32fd56680bdca25d19d23e91a02dc
Minh Khoi Pham <max.pham@mail.utoronto.ca>
2019-03-24 06:30:57 -0400

    changes

--- todo.py
+++ todo.py
@@ -16,18 +16,14 @@ class Todo(Resource):
 
     def get(self, code):
         # try:
-        # print(code)
-        # cursor.execute('''SELECT courseCode, title, hours, summary,
-        #                         prerecquisites, exclusions, distribution,
-        #                         breadth, program FROM courses WHERE courseCode=?''', (code,))
-        # course = cursor.fetchone()
-        #
-        # return course, 200
-        message = {"recipe": {"publisher": "Closet Cooking", "f2f_url": "http://food2fork.com/view/35382", "ingredients": ["2 jalapeno peppers, cut in half lengthwise and seeded", "2 slices sour dough bread", "1 tablespoon butter, room temperature", "2 tablespoons cream cheese, room temperature", "1/2 cup jack and cheddar cheese, shredded", "1 tablespoon tortilla chips, crumbled\n"], "source_url": "http://www.closetcooking.com/2011/04/jalapeno-popper-grilled-cheese-sandwich.html", "recipe_id": "35382", "image_url": "http://static.food2fork.com/Jalapeno2BPopper2BGrilled2BCheese2BSandwich2B12B500fd186186.jpg", "social_rank": 100.0, "publisher_url": "http://closetcooking.com", "title": "Jalapeno Popper Grilled Cheese Sandwich"}}
-        # Making the message looks good
-        resp = jsonify(message)
-        # Returning the object
-        return resp, 200
+        print(code)
+        cursor.execute('''SELECT courseCode, title, hours, summary,
+                                prerecquisites, exclusions, distribution,
+                                breadth, program FROM courses WHERE courseCode=?''', (code,))
+        course = cursor.fetchone()
+
+        return course, 200
+
 
         # except Exception as e:
         #     # Roll back any change if something goes wrong
@@ -37,5 +33,3 @@ class Todo(Resource):
 class Root(Resource):
     def get(self):
         return {"recipe": {"publisher": "Closet Cooking", "f2f_url": "http://food2fork.com/view/35382", "ingredients": ["2 jalapeno peppers, cut in half lengthwise and seeded", "2 slices sour dough bread", "1 tablespoon butter, room temperature", "2 tablespoons cream cheese, room temperature", "1/2 cup jack and cheddar cheese, shredded", "1 tablespoon tortilla chips, crumbled\n"], "source_url": "http://www.closetcooking.com/2011/04/jalapeno-popper-grilled-cheese-sandwich.html", "recipe_id": "35382", "image_url": "http://static.food2fork.com/Jalapeno2BPopper2BGrilled2BCheese2BSandwich2B12B500fd186186.jpg", "social_rank": 100.0, "publisher_url": "http://closetcooking.com", "title": "Jalapeno Popper Grilled Cheese Sandwich"}}
-
-api.add_resource(Root, '/')
