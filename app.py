8d509257d225e84fb75f21f18e9935bff6a256e5
Minh Khoi Pham <max.pham@mail.utoronto.ca>
2019-03-24 06:32:50 -0400

    changes

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from flask_restful import Api
 
-from todo import Todo
+from todo import Todo, Root
 
 app = Flask(__name__)
 api = Api(app)
 
 api.add_resource(Todo, "/get/<code>")
+api.add_resource(Root,"/")
 
 if __name__ == "__main__":
     app.run()
